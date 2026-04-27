c9baa4774a09cd5a40f6dfcc568b31f8948dec5e
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 13:17:47 -0300

    chore: projeto console .NET 8
    intent: chore

--- obj/Debug/net8.0/api_rest_de_health_check_fd32d2.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_rest_de_health_check_fd32d2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0989acafd76e49aad202e2065ad3bf9ff9f3e29f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1690e675a6a80d6fd924a40d8b835245ea9cb8fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
