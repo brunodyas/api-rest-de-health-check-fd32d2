51a9768c1e75a1a084bb6045866869a886a90fbd
BrunoDyas <brunodyas@users.noreply.github.com>
2026-04-27 13:16:23 -0300

    feat: configuração estática
    intent: feature

--- obj/Debug/net8.0/api_rest_de_health_check_fd32d2.AssemblyInfo.cs
+++ obj/Debug/net8.0/api_rest_de_health_check_fd32d2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70b03f81c3551de28443a6e3608bcf1532e3af7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829b9694c3a815a7acd4f07d3376276abe792548")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_rest_de_health_check_fd32d2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
